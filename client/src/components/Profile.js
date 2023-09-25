1e030ce06a819d9129f7d8488b1782dddb8cf5e5
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-09-24 21:13:05 -0700

    some markup for profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -27,4 +27,24 @@ export default function Profile({ loggedIn }) {
             }
         `, { id: loggedIn }).then(response => setCurrentUser(response.data.userById));
     }, []);
+
+    return (
+        <>
+            <h1>
+                {currentUser.username}'s Profile
+            </h1>
+            <div id='profileFlexContainer'>
+                <div className='profileDiv'>
+                    <h2>
+                        Conversations
+                    </h2>
+                </div>
+                <div className='profileDiv'>
+                    <h2>
+                        Info
+                    </h2>
+                </div>
+            </div>
+        </>
+    )
 }
