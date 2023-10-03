aeb5c7c718bf328ceda9427db45e6829056385b1
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-10-03 14:09:44 -0700

    Some style experimentation

--- client/src/components/ViewConversation.js
+++ client/src/components/ViewConversation.js
@@ -12,54 +12,58 @@ export default function ViewConversation({ loggedIn, conversation, onAddRemark, 
     }
 
     return (
-        <>
-            <h1>
-                Conversation
-            </h1>
-            <h2>
-                {conversation.title}
-            </h2>
-            <h3>
-                Topics
-            </h3>
-            <ul>
-                {conversation.topics.map((topic, index) =>
-                    <li key={index}>
-                        {topic}
-                    </li>    
-                )}
-            </ul>
-            <h3>
-                Participants
-            </h3>
-            <ul>
-                {conversation.participants.map((participant, index) => 
-                    <li key={participant.id}>
-                        {participant.username}
-                    </li>
-                )}
-            </ul>
-            <h1>
-                Remarks
-            </h1>
-            <ul>
-                {conversation.remarks.map(remark =>
-                    <li key={remark.id}>
-                        <Remark 
-                            remark={remark} 
-                            onSaveComment={(body, remarkId) => onSaveComment(body, remarkId)} 
-                        />
-                    </li>    
-                )}
-            </ul>
-            {loggedIn && !composing && <button onClick={() => setComposing(true)}>Add Remark</button>}
-            {composing && <>
-                <textarea 
-                    value={newRemark}
-                    onChange={e => setNewRemark(e.target.value)}
-                />
-                <button onClick={handleSaveRemarkClick}>Save</button>
-            </>}
-        </>
+        <>{conversation && <>
+            <div className="card">
+                <div className="card">
+                    <h1>
+                        Conversation
+                    </h1>
+                    <h2>
+                        {conversation.title}
+                    </h2>
+                    <h3>
+                        Topics
+                    </h3>
+                    <ul>
+                        {conversation.topics.map((topic, index) =>
+                            <li key={index}>
+                                {topic}
+                            </li>    
+                        )}
+                    </ul>
+                    <h3>
+                        Participants
+                    </h3>
+                    <ul>
+                        {conversation.participants.map((participant, index) => 
+                            <li key={participant.id}>
+                                {participant.username}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                <h1>
+                    Remarks
+                </h1>
+                <ul>
+                    {conversation.remarks.map(remark =>
+                        <li key={remark.id}>
+                            <Remark 
+                                remark={remark} 
+                                onSaveComment={(body, remarkId) => onSaveComment(body, remarkId)} 
+                            />
+                        </li>    
+                    )}
+                </ul>
+                {loggedIn && !composing && <button onClick={() => setComposing(true)}>Add Remark</button>}
+                {composing && <>
+                    <textarea 
+                        value={newRemark}
+                        onChange={e => setNewRemark(e.target.value)}
+                    />
+                    <button onClick={handleSaveRemarkClick}>Save</button>
+                </>}
+            </div>
+        </>}</>
     )
 }
